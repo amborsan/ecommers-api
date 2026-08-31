346db49b81fa0ffb4ae2727f79707d77a589f05a
ali <ali.alborsan@dci-student.org>
2026-08-31 12:46:47 +0200

    Modified to easy code in some files

--- src/models/productsModel.js
+++ src/models/productsModel.js
@@ -1,20 +1,26 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
-const productsScheme = mongoose.Schema({
-    name:{type:String, required:true, trim:true},
-    description:{type:String, required:true, trim:true},
-    price:{type:Number, required:true},
-    discount:{type:Number, required:true},
-    category: {type:String, required:true, trim:true},
-    variants: {type:Array, required:false},
-    inStock:{type:Boolean},
-    tages:{type:Array, required:false},
-    brand:  {type:String, required:false, trim:true},
-    images:{type:Array, required:true, trim:true},
-
-
-}, {
-    timestamps: true
-});
+const productsScheme = mongoose.Schema(
+  {
+    name: { type: String, required: true, trim: true },
+    description: { type: String, required: true, trim: true },
+    price: { type: Number, required: true },
+    discount: { type: Number, required: true },
+    category: { type: String, required: true, trim: true },
+    variants: { type: Array, required: false },
+    inStock: { type: Boolean },
+    tages: { type: Array, required: false },
+    brand: { type: String, required: false, trim: true },
+    images: { type: Array, required: true, trim: true },
+  },
+  {
+    timestamps: true,
+  },
+);
 // const productsModel = mongoose.model('products',productsScheme, 'products' )
-export default mongoose.model('productsModel',productsScheme, 'products' );
+const productsModel = mongoose.model(
+  "productsModel",
+  productsScheme,
+  "products",
+);
+export default productsModel;
