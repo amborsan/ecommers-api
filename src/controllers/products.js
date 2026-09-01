691653e96bfb99aeba8210eb691aa62622690ed4
I-Chieh Liu <travisliu78@gmail.com>
2026-09-01 11:38:58 +0200

    feat: update product deletion and patching functionality in controllers and routes

--- src/controllers/products.js
+++ src/controllers/products.js
@@ -74,11 +74,30 @@ export async function getProductyId(req, res, next) {
 }
 
 // Delete products controller
- export async function deleteProduct (req, res)  {
-    try{
-const product = await productsModel.findByIdAndDelete(req.params.id);
-res.status(201).json({message: "Product deleted successfully"});
-    }catch(error){
-        res.status(400).json({message: error.message})
+export async function deleteProduct(req, res) {
+  try {
+    const product = await productsModel.findByIdAndDelete(req.params.id);
+    res.status(201).json({ message: "Product deleted successfully" });
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+}
+
+// Update selected product（PATCH）
+export async function patchProduct(req, res) {
+  try {
+    const updatedProduct = await productsModel.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true, runValidators: true },
+    );
+
+    if (!updatedProduct) {
+      return res.status(404).json({ message: "Product not found" });
     }
+
+    res.status(200).json(updatedProduct);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
 }
